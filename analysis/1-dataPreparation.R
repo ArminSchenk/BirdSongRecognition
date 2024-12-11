342f7b09b495c4b1dfd6b36c6d22596a11ebe973
Armin Schenk <armin.schenk99@gmail.com>
2024-12-11 20:10:48 +0100

    Add random selection of 10 second chunks and padding for too short audio

--- analysis/1-dataPreparation.R
+++ analysis/1-dataPreparation.R
@@ -1,4 +1,8 @@
 library(torchaudio)
+library(torch)
+
+# Seed
+set.seed(42)
 
 # Mel spectrogram parameters
 n_seconds <- 10
@@ -18,8 +22,8 @@ path_to_data <- "data/birdclef2024/"
 rating_treshold <- 3
 classes_treshold <- 5
 
-# Calculate time dimension of resulting spectra
-n_time_bins <- ((n_seconds*sample_rate - win_length)%/%hop_length)+3
+target_length <- n_seconds*sample_rate
+n_time_bins <- ((target_length - win_length)%/%hop_length)+3
 
 # Load metadata
 metadata <- read.csv(paste0(path_to_data, "train_metadata.csv"))
@@ -44,6 +48,7 @@ mel_transformer <- transform_mel_spectrogram(sample_rate = sample_rate,
 spectra <- array(0, dim=c(nrow(metadata), 1, n_mels, n_time_bins))
 rm_indices <- c()
 i <- 0
+print(nrow(metadata))
 for(filename in metadata[,"filename"]) {
   i <- i+1
   if(i %% 100 == 0) print(i)
@@ -58,12 +63,20 @@ for(filename in metadata[,"filename"]) {
     rm_indices <- c(rm_indices, i)
     next
   }
-  if(dim(audio)[2] < n_seconds*sample_rate) {
-    print(paste0(i, ": Audio file is too short: ", filename))
-    rm_indices <- c(rm_indices, i)
-    next
+  audio_torch <- transform_to_tensor(audio)[[1]]
+  audio_length <- dim(audio_torch)[2]
+  dif_length <- abs(target_length - audio_length)
+  
+  # Padding or Cropping
+  if(audio_length < target_length) {
+    left_pad <- sample.int(dif_length, 1)
+    right_pad <- dif_length - left_pad
+    audio_torch <- torch_cat(list(torch_zeros(1, left_pad), audio_torch, torch_zeros(1, right_pad)), dim = 2L)
+  } else if(audio_length > target_length) {
+    idx <- sample.int(dif_length+1, 1)
+    audio_torch <- audio_torch[,idx:(idx-1+target_length)]
   }
-  audio_torch <- transform_to_tensor(audio)[[1]][,1:(n_seconds*sample_rate)]
+  
   spectra[i,,,] <- as.array(mel_transformer(audio_torch))
 }
 
@@ -89,3 +102,35 @@ for(i in 1:nrow(metadata)) {
   if(max-min >= 1e-10) spectra[i,,,] <- (spectra[i,,,] - min) / (max - min)
   else spectra[i,,,] <- spectra[i,,,] - min
 }
+
+
+rm(audio,
+   audio_length,
+   audio_torch,
+   classes,
+   classes_treshold,
+   dif_length,
+   f_max,
+   f_min,
+   filename,
+   hop_length,
+   i,
+   idx,
+   indices,
+   left_pad,
+   max,
+   mel_transformer,
+   min,
+   n_fft,
+   n_mels,
+   n_seconds,
+   n_time_bins,
+   path_to_data,
+   power,
+   rating_treshold,
+   right_pad,
+   rm_indices,
+   sample_rate,
+   target_length,
+   win_length)
+
